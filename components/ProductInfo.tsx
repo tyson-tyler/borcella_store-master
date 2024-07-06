6f3d0f66ff51a74c6baa6b971afd41d550865782
tyson-tyler <125717208+tyson-tyler@users.noreply.github.com>
2024-07-06 10:55:34 +0530

    Update ProductInfo.tsx
    dfaf

--- components/ProductInfo.tsx
+++ components/ProductInfo.tsx
@@ -102,6 +102,13 @@ const ProductInfo = ({ productInfo }: { productInfo: ProductType }) => {
       >
         Add To Cart
       </button>
+      <Link href={productInfo.description}>
+          <button
+        className="outline text-base-bold py-3 rounded-lg hover:bg-black hover:text-white"
+      >
+        Buy Now
+      </button>
+        </Link>
     </div>
   );
 };
